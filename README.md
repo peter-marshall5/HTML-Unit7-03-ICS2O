5d2c6aa9c036cdc13da25fc3c87e330ca77b416a
Peter Marshall <peter.marshall@ocsbstudent.ca>
2021-02-19 17:22:58 +0000

    Add linter

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unit 7-03
+
+[![linter](https://github.com/peter-marshall5/HTML-Unit7-03-ICS2O/workflows/linter/badge.svg)](https://github.com/marketplace/actions/super-linter)
---
+++ script.js
@@ -0,0 +1,32 @@
+const input = document.getElementById('age')
+const button = document.getElementById('check')
+
+function onclick () {
+  if (input.value === '') {
+    alert('Please enter your age.')
+    return
+  }
+  const age = parseFloat(input.value)
+  if (isNaN(age)) {
+    alert('You entered an invalid number.')
+  }
+  if (age % 1 > 0) {
+    alert('Your age must not contain any decimals.')
+    return
+  }
+  if (age < 0) {
+    alert('Your age can not be negative.')
+    return
+  }
+  if (age >= 17) {
+    alert('You can watch R rated movies.')
+  } else if (age >= 13) {
+    alert('You can watch PG-13 rated movies.')
+  } else if (age >= 5) {
+    alert('You can watch G or PG rated movies.')
+  } else {
+    alert('You\'re too young for most movies.')
+  }
+}
+
+button.addEventListener('click', onclick)
